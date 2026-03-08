ae938499cbf51ee32e8957bfed7c118f2603d38e
Kevin <kern3985@gmail.com>
2026-03-08 12:27:51 -0700

    boilerplate

---
+++ testsrcriptmaker/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {Tabs} from '@mui/material';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Tabs></Tabs>
+      hi
+    </>
+  )
+}
+
+export default App
